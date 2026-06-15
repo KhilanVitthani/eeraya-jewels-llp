0d3ea6521e3036ac26f00a48f02c9cdccdc1757e
Khilan Vitthani <khilanvithani0080@gmail.com>
2026-06-16 02:31:28 +0530

    Deploy website update

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"42d3d75a56efe1a2e9902f52dc8006099c45d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "384761914" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "803909120" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
